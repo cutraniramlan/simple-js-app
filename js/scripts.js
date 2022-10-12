07d2e29d9353104bd3718fb24529277c67b100a3
cutraniramlan <cutraniramlan@gmail.com>
2022-10-12 11:34:55 +0200

    Task 1.2 Javascript basic 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-alert('Hello world');
-let favoriteFood = 'Rendang, this is Indonesian food which contain meat with various types of spices';
-document.write(favoriteFood);
+var pokemonList = [
+
+{
+name: "Bulbasaur",
+height: 7,
+type : ['grass', 'poison']
+},
+
+
+{
+name: "Charmander",
+height: 2,
+type: 'fire'
+},
+
+{
+name: "Pikachu",
+height: 1,
+type: 'electric'
+}
+
+];
