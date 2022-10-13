495d72160366af395db48b9d746604b4ddb6e8d1
cutraniramlan <cutraniramlan@gmail.com>
2022-10-14 01:13:18 +0200

    Task 1.3 JavaScript Basics Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,15 @@
 var pokemonList = [
-
-{
-name: "Bulbasaur",
-height: 7,
-types : ['grass', 'poison']
-},
-
-
-{
-name: "Charmander",
-height: 2,
-types: 'fire'
-},
-
-{
-name: "Pikachu",
-height: 1,
-types: 'electric'
-}
-
+    {name: "Bulbasaur", height: 2, types : ['grass', 'poison']},
+    {name: "Charmander", height: 2, types: 'fire'},
+    {name: "Pikachu", height: 1, types: 'electric'}
 ];
+
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height >=2){
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") -  Waw, that's big! " + "<br>")
+    }
+    else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "<br>")
+}
+}
