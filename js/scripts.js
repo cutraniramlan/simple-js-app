17488c57513f8a9ccedccda268d67aca5a8c019e
cutraniramlan <cutraniramlan@gmail.com>
2022-10-18 11:18:06 +0200

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,27 @@ var pokemonList = [
     {name: "Pikachu", height: 1, types: 'electric'}
 ];
 
-  pokemonList.forEach(function(pokemon){
-    document.write('Pokemon Name: ' + pokemon.name + ',' + ' height: ' + pokemon.height + ',' + ' types: ' + pokemon.types + '<br>' )
-    });
+      let pokemonRepository = (function () {
+        let pokemonList = [];
+      
+        function add(pokemon) {
+          pokemonList.push(pokemon);
+        }
+      
+        function getAll() {
+          return pokemonList;
+        }
+      
+        return {
+          add: add,
+          getAll: getAll
+        };
+      })();
 
+      console.log(pokemonRepository.getAll()); [pokemonList]
+      pokemonList [pokemonRepository.add({ name: 'Bulbasaur' })];
+      console.log(pokemonRepository.getAll()); [ { name: 'Bulbasaur' } ]
+
+pokemonList.forEach(function(pokemon){
+     document.write('Pokemon Name: ' + pokemon.name + ',' + ' height: ' + pokemon.height + ',' + ' types: ' + pokemon.types + '<br>' )
+        });
