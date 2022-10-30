322dd7ea58539df72584f48bc8b5b5f24efb0199
cutraniramlan <cutraniramlan@gmail.com>
2022-10-30 17:26:00 +0100

    add application layouts using Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,23 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
   function addListItem(pokemon) {
-    let pokemonList = document.querySelector(".pokemon-list");
+    let pokemonList = document.querySelector(".list-group");
     let listpokemon = document.createElement("li");
+    listpokemon.classList.add("group-list-item");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
-    button.classList.add("button-class");
+
+    button.setAttribute("data-toggle", "modal");
+    button.setAttribute("data-target", "#exampleModal");
+    button.classList.add("btn");
+    button.classList.add("btn-primary");
+
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
+
     addEventListener(button, pokemon);
   }
+
   function addEventListener(button, pokemon) {
     button.addEventListener("click", (event) => {
       showDetails(pokemon);
@@ -54,8 +62,9 @@ let pokemonRepository = (function () {
       })
       .then(function (details) {
         item.imageUrl = details.sprites.front_default;
+        item.weight = details.weight;
         item.height = details.height;
-        item.types = details.types;
+        item.types = details.types.map((type) => type.type.name).join(",");
       })
       .catch(function (e) {
         console.error(e);
@@ -63,11 +72,11 @@ let pokemonRepository = (function () {
   }
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      showModal(pokemon);
       console.log(pokemon);
+      showModal(pokemon);
     });
   }
-
+  /* 
   function showModal(pokemon) {
     let modalContainer = document.querySelector("#modal-container");
     modalContainer.innerHTML = "";
@@ -127,6 +136,65 @@ let pokemonRepository = (function () {
     let modalContainer = document.querySelector("#modal-container");
     if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
       hideModal();
+    }
+  }); */
+
+  function showModal(pokemon) {
+    let modalBody = $(".modal-body");
+    let modalTitle = $(".modal-title");
+    let modalHeader = $(".modal-header");
+
+    modalTitle.empty();
+    modalBody.empty();
+
+    let pokemonName = $("<h1>" + pokemon.name + "</h1>");
+
+    /*     let pokemonImageFront = $('<img class="modal-img" style=width:50%">');
+    pokemonImageFront.attr("src", pokemon.imageUrlFront);
+
+    let pokemonImageBack = $('<img class="modal-img" style=width:50%">');
+    pokemonImageBack.attr("src", pokemon.imageUrlBack); */
+
+    let pokemonImage = $('<img class="modal-img" style=width:75%">');
+    pokemonImage.attr("src", pokemon.imageUrl);
+
+    let pokemonHeight = $("<p>" + "height : " + pokemon.height + "</p>");
+
+    let pokemonWeight = $("<p>" + "weight : " + pokemon.weight + "</p>");
+
+    let pokemonTypes = $("<p>" + "types : " + pokemon.types + "</p>");
+
+    /*   let pokemonAbilities = $(
+      "<p>" + "abilities : " + pokemon.abilities + "</p>"
+    );
+ */
+    modalTitle.append(pokemonName);
+    /* modalBody.append(pokemonImageFront);
+    modalBody.append(pokemonImageBack); */
+    modalBody.append(pokemonImage);
+    modalBody.append(pokemonHeight);
+    modalBody.append(pokemonWeight);
+    modalBody.append(pokemonTypes);
+    /*    modalBody.append(pokemonAbilities); */
+  }
+
+  function hideModal() {
+    let modalContainer = document.querySelector("#modal-container");
+    modalContainer.classList.remove("is-visible");
+  }
+
+  window.addEventListener("keydown", (e) => {
+    let modalContainer = document.querySelector("#modal-container");
+    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+      hideModal();
+    }
+  });
+
+  pokemonList.forEach(function (pokemon) {
+    if (pokemon.innerText.toUpperCase().indexOf(inputValue) > -1) {
+      pokemon.style.display = "";
+    } else {
+      pokemon.style.display = "none";
     }
   });
 
